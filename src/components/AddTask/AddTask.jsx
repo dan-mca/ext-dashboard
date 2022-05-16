b9af033dac826591e5c0f597e5fff270267500ca
Dan McAuley <danmcauley86@gmail.com>
2022-05-16 20:56:57 +0100

    Added localStorage logic to AddTask

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,24 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AddTaskContainer, AddTaskInput, AddTaskButton } from './AddTask.styled'
 
 const AddTask = () => {
   const [inputValue, setInputValue] = useState('')
-  const [tasks, setTasks] = useState([])
-
-  const createTaskObject = (taskId, taskText, isChecked) => ({id: taskId, taskText, isChecked: isChecked})
-  const addTask = (task) => setTasks([...tasks, createTaskObject(tasks.length, task.taskText, false)])
+  const [tasks, setTasks] = useState(() => {
+    const savedtasks = localStorage.getItem('tasks')
+    if (savedtasks) {
+      return JSON.parse(savedtasks);
+    } else {
+      return [];
+    }
+  })
 
   const handleChange = e => setInputValue(e.target.value)
 
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks])
+
   const handleInput = e => {
-    if(e.key === 'Enter' && inputValue.length > 0) {
-      setInputValue('')
-      localStorage.setItem('input', inputValue)
-      addTask({taskText: inputValue})
+    if (e.key === 'Enter' && inputValue.length > 0) {
+      setTasks([...tasks, {id: tasks.length + 1, text: inputValue.trim()}]);
+      setInputValue("");
     }
   }
-  
-  console.log(tasks)
+
 
   return (
     <AddTaskContainer>
